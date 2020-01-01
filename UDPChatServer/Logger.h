dbe9f5b6c2453625dacebf66cfce1c45ed357553
Aditya Mudaliar <mudaliaraditya@gmail.com>
2020-01-01 21:13:30 +0530

    Logger.h Changes

--- UDPChatServer/Logger.h
+++ UDPChatServer/Logger.h
@@ -59,6 +59,79 @@
 
 
 
+#define DO_THING(cToBeLogged, ...)    switch(lnEnum){ case 0: goto loggingout;break; default:  ACTUALLOG(lnEnum & lnBitCheck,cToBeLogged, ##__VA_ARGS__) ;lnBitCheck=lnBitCheck<<1;break; }
+#define DO_THING_2(cToBeLogged, ...)  DO_THING(cToBeLogged, ##__VA_ARGS__); ;DO_THING(cToBeLogged, ##__VA_ARGS__)
+#define DO_THING_4(cToBeLogged, ...)  DO_THING_2(cToBeLogged, ##__VA_ARGS__); ;DO_THING_2(cToBeLogged, ##__VA_ARGS__)
+#define DO_THING_8(cToBeLogged, ...) DO_THING_4(cToBeLogged, ##__VA_ARGS__); ;DO_THING_4(cToBeLogged, ##__VA_ARGS__)
+#define DO_THING_16(cToBeLogged, ...) DO_THING_8(cToBeLogged, ##__VA_ARGS__); DO_THING_8(cToBeLogged, ##__VA_ARGS__)
+
+
+enum LOG_LEVEL{INFO = 1 ,DEBUG = 2,WARN = 4,ERROR = 8,FATAL = 16,OUT = 32};
+
+#define ACTUALLOG(nEnum,cToBeLogged, ...)\
+{\
+{\
+    \
+    char lcVar[1500];               \
+    switch(nEnum )                   \
+    {                               \
+        case INFO:                  \
+        snprintf(lcVar,1500," | %s | %s","INFO",cToBeLogged);\
+        TESTLOG( lcVar, ##__VA_ARGS__);  \
+        lnEnum=lnEnum - INFO;               \
+        break;                      \
+        case DEBUG:                 \
+        snprintf(lcVar,1500," | %s | %s","DEBUG",cToBeLogged);\
+        TESTLOG( lcVar, ##__VA_ARGS__);  \
+        lnEnum=lnEnum - DEBUG;              \
+        break;                      \
+        case WARN:                  \
+        snprintf(lcVar,1500," | %s | %s","WARN",cToBeLogged);\
+        TESTLOG( lcVar, ##__VA_ARGS__);  \
+        lnEnum=lnEnum - WARN;               \
+        break;                      \
+        case OUT:                   \
+        snprintf(lcVar,1500," | %s | %s","WARN",cToBeLogged);\
+        TESTOUT( lcVar, ##__VA_ARGS__);  \
+        lnEnum=lnEnum - OUT;                \
+        break;                      \
+        case ERROR:                 \
+        snprintf(lcVar,1500," | %s | %s","ERROR",cToBeLogged);\
+        LOG_LOGGER( lcVar, ##__VA_ARGS__)\
+        lnEnum = lnEnum - ERROR;              \
+        break;                      \
+        case FATAL:                 \
+        snprintf(lcVar,1500," | %s | %s","FATAL",cToBeLogged);\
+        LOG_LOGGER( lcVar, ##__VA_ARGS__)\
+        lnEnum=lnEnum - FATAL;              \
+        break;                      \
+    }                               \
+    \
+                                    \
+    \
+}\
+\
+}
+
+#define LOG(nEnum,cToBeLogged, ...) \
+{\
+                   \
+    int lnBitCheck = 1;\
+    int lnEnum = nEnum;\
+    ACTUALLOG(lnEnum,cToBeLogged, ##__VA_ARGS__);\
+    switch(lnEnum)\
+    {   case 0:\
+        break;\
+        default:\
+        DO_THING_8(cToBeLogged, ##__VA_ARGS__);   \
+        break;                           \
+    }                                \
+loggingout:    \
+    int n;\
+    \
+}                            
+
+
 
 long getMicrotime();
 
